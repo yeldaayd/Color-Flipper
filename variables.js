54b1cca74c0076650ab613686f613d72c3265fcb
yeldaayd <yeldaa63@gmail.com>
2025-03-10 23:45:49 +0300

    ilk commit

---
+++ variables.js
@@ -0,0 +1,12 @@
+
+
+
+let text = "victor"
+let isStudent = true;
+let hobbies;
+let girlfriends = null;
+
+console.log(typeof text);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof girlfriends);
